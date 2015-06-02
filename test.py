d6be44b2f1c033f9ece816d24f0ae60a04fa3a3d
AzDev <elman.liutfaliiev@gmail.com>
2015-06-03 02:01:36 +0300

    [HW-001] hipChat message parser
    added unit tests using doctest
    added multithreading using concurrent
    you can manage threads changing max_workers parameter. If you will use
    python 2x, you have to install futures from the whl package.

--- test.py
+++ test.py
@@ -10,8 +10,41 @@
 
 import re
 import urllib2
+import concurrent.futures
 
 class hipChatMessageParser(object):
+    """
+    >>> hipTest_1 = hipChatMessageParser('@chris; you around? <a href="http://www.google.com">google site</a>', False)
+    >>> hipTest_1.get_mentions
+    ['chris']
+    >>> hipTest_1.get_rtf_url
+    ['<a href="http://www.google.com">google site</a>']
+    >>> hipTest_1.get_url
+    'http://www.google.com'
+    >>> hipTest_1.get_title('http://www.google.com')
+    'Google'
+    >>> hipTest_1.get_title('ww.g.om')
+    'unable to read title'
+    >>> hipTest_1.get_links
+    [{'url': '<a href="http://www.google.com">google site</a>', 'title': 'Google'}]
+    >>> hipTest_1.string = "<a hef=>gogle"
+    >>> hipTest_1.get_links
+    >>> hipTest_1 = hipChatMessageParser('Good morning! (coffee) () (4ai) (un:own) (itwillben0tparsed)')
+    >>> hipTest_1.get_emoticons
+    ['coffee', '4ai']
+    >>> hipTest_1 = hipChatMessageParser('@chris you around?')
+    >>> hipTest_1.to_json()
+    {'mentions': ['chris']}
+    >>> hip_parsed_2 = hipChatMessageParser('Good morning! (megusta) (coffee)')
+    >>> hip_parsed_2.to_json()
+    {'emoticons': ['megusta', 'coffee']}
+    >>> hip_parsed_3 = hipChatMessageParser('Olympics are starting soon; <a href="http://www.nbcolympics.com/" target="_blank"><b><span style="color:red;text-decoration:none">MailScanner has detected a possible fraud attempt from "www.nbcolympics.com" claiming to be</span></b><span style="color:#3572b0;text-decoration:none"> http://www.nbcolympics.com"</span></a>')
+    >>> hip_parsed_3.to_json()
+    {'links': [{'url': '<a href="http://www.nbcolympics.com/" target="_blank"><b><span style="color:red;text-decoration:none">MailScanner has detected a possible fraud attempt from "www.nbcolympics.com" claiming to be</span></b><span style="color:#3572b0;text-decoration:none"> http://www.nbcolympics.com"</span></a>', 'title': 'NBC Olympics | Home of the 2016 Olympic Games in Rio'}]}
+    >>> hip_parsed_4 = hipChatMessageParser('@bob @john (success) such a cool feature; <a href="https://twitter.com/jdorfman/status/430511497475670016" target="_blank"><span style="color:#3572b0;text-decoration:none">https://twitter.com/<wbr>jdorfman/status/<wbr>430511497475670016"</span></a>')
+    >>> hip_parsed_4.to_json()
+    {'mentions': ['bob', 'john'], 'emoticons': ['success'], 'links': [{'url': '<a href="https://twitter.com/jdorfman/status/430511497475670016" target="_blank"><span style="color:#3572b0;text-decoration:none">https://twitter.com/<wbr>jdorfman/status/<wbr>430511497475670016"</span></a>', 'title': 'Justin Dorfman on Twitter: &quot;nice @littlebigdetail from @HipChat (shows hex colors when pasted in chat). http://t.co/7cI6Gjy5pq&quot;'}]}
+    """
 
     def __init__(self, string, autoparsing=True):
         self.string = string
@@ -97,26 +130,27 @@ class hipChatMessageParser(object):
         result_str = p.subn('', string)[0]
         return result_str
 
+# you can use this function if you need only print output  
+def multithread_json_printer(hip_chat_message):
+    hip_parsed = hipChatMessageParser(hip_chat_message)
+    print "input is:", hip_chat_message
+    print "JSON output is:", hip_parsed.to_json()
+    print ""
 
+# u can operate result of parsing in the body of this function
+# if u need to do smth with it using multithreading
+def multithread_json_parser(hip_chat_message):
+    hip_parsed = hipChatMessageParser(hip_chat_message)
+    print "input is:", hip_chat_message
+    json_str = hip_parsed.to_json()
+    #N! do smth with json_str.....
+    #N!
+    #N! or return as a result to shared list:
+    # return hip_parsed.to_json()
 
-# Sample 1:
-hip_parsed_1 = hipChatMessageParser('@chris you around?')
-print "input is:", "@chris you around?"
-print "JSON output is:", hip_parsed_1.to_json()
-print ""
-# Sample 2:
-hip_parsed_2 = hipChatMessageParser('Good morning! (megusta) (coffee)')
-print "input is:", "Good morning! (megusta) (coffee)"
-print "JSON output is:", hip_parsed_2.to_json()
-print ""
-# Sample 3:
-# In third and forth example there are a links with a lot of tags so they were get as is
-hip_parsed_3 = hipChatMessageParser('Olympics are starting soon; <a href="http://www.nbcolympics.com/" target="_blank"><b><span style="color:red;text-decoration:none">MailScanner has detected a possible fraud attempt from "www.nbcolympics.com" claiming to be</span></b><span style="color:#3572b0;text-decoration:none"> http://www.nbcolympics.com"</span></a>')
-print "input is:", 'Olympics are starting soon; <a href="http://www.nbcolympics.com/" target="_blank"><b><span style="color:red;text-decoration:none">MailScanner has detected a possible fraud attempt from "www.nbcolympics.com" claiming to be</span></b><span style="color:#3572b0;text-decoration:none"> http://www.nbcolympics.com"</span></a>'
-print "JSON output is:", hip_parsed_3.to_json()
-print ""
-# Sample 4:
-hip_parsed_4 = hipChatMessageParser('@bob @john (success) such a cool feature; <a href="https://twitter.com/jdorfman/status/430511497475670016" target="_blank"><span style="color:#3572b0;text-decoration:none">https://twitter.com/<wbr>jdorfman/status/<wbr>430511497475670016"</span></a>')
-print "input is:", '@bob @john (success) such a cool feature; <a href="https://twitter.com/jdorfman/status/430511497475670016" target="_blank"><span style="color:#3572b0;text-decoration:none">https://twitter.com/<wbr>jdorfman/status/<wbr>430511497475670016"</span></a>'
-print "JSON output is:", hip_parsed_4.to_json()
-print ""
+if __name__ == "__main__":
+    hipChat_messages = ['@chris you around?', 'Good morning! (megusta) (coffee)']
+
+    # returns in the order given
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(multithread_json_printer, hipChat_messages)
